7260e58b025fe07e8e3880d2887891a19e2af2a4
Dimitri Couwenberg <couwenberg@gmail.com>
2017-05-24 22:40:55 +1000

    update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,29 @@ const router = {
     }
 };
 
-//import Oidc from 'oidc-client';
-// userManager = new Oidc.UserManager(null);
 
+import Oidc from 'oidc-client';
+
+var settings = {
+    authority: 'http://localhost:5000/oidc',
+    client_id: 'js.tokenmanager',
+    redirect_uri: 'http://localhost:5000/user-manager-sample.html',
+    post_logout_redirect_uri: 'http://localhost:5000/user-manager-sample.html',
+    response_type: 'id_token token',
+    scope: 'openid email roles',
+
+    popup_redirect_uri:'http://localhost:5000/user-manager-sample-popup-signin.html',
+    popup_post_logout_redirect_uri:'http://localhost:5000/user-manager-sample-popup-signout.html',
+
+    silent_redirect_uri:'http://localhost:5000/user-manager-sample-silent.html',
+    automaticSilentRenew:false,
+    //silentRequestTimeout:10000,
+
+    filterProtocolClaims: true,
+    loadUserInfo: true
+};
+
+var mgr = new Oidc.UserManager(settings);
 
 ReactDOM.render(
     React.createElement(Router, { ...router }),
